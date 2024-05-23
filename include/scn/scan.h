1b0d505921660017c4e5d353105d8c239b21ae3a
Elias Kosunen <elias.kosunen@gmail.com>
2024-05-23 15:24:36 +0300

    Qualify a name in a noexcept clause for MSVC

--- include/scn/scan.h
+++ include/scn/scan.h
@@ -599,9 +599,10 @@ struct SCN_TRIVIAL_ABI expected_operations_base<T, E, false>
 private:
     template <typename Other>
     void construct_common(Other&& other) noexcept(
-        noexcept(construct(std::forward<Other>(other).get_value())) &&
-        noexcept(
-            construct_unexpected(std::forward<Other>(other).get_unexpected())))
+        noexcept(expected_storage_base<T, E>::construct(
+            std::forward<Other>(other).get_value())) &&
+        noexcept(expected_storage_base<T, E>::construct_unexpected(
+            std::forward<Other>(other).get_unexpected())))
     {
         if (other.has_value()) {
             this->construct(std::forward<Other>(other).get_value());
